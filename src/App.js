3969691852990c26af838d3b6b52a8f8f67563c3
Mohit Ande <andemohit@gmail.com>
2020-05-30 18:23:14 +0530

    installed google analystics

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Cards, Charts, CountryPicker } from './components';
 import styles from './App.module.css';
 import { fetchData } from './api';
+import ReactGA from 'react-ga';
 
 import coronaImage from './images/image.png';
 
@@ -12,6 +13,12 @@ class App extends React.Component {
     country: ''
   }
 
+  initializeReactGA() {
+    ReactGA.initialize('UA-167987202-1');
+    ReactGA.pageview('');
+  }
+
+
   async componentDidMount() {
     const fetchedData = await fetchData();
     this.setState({ data: fetchedData });
@@ -20,17 +27,17 @@ class App extends React.Component {
   handleCountryChange = async (country) => {
     const fetchedData = await fetchData(country);
     this.setState({ data: fetchedData, country: country });
-    
+
   }
 
   render() {
     const { data, country } = this.state
     return (
       <div className={styles.container}>
-        <img className={styles.image} src={coronaImage} alt="COVID-19"/>
+        <img className={styles.image} src={coronaImage} alt="COVID-19" />
         <Cards data={data} />
-        <CountryPicker handleCountryChange={this.handleCountryChange}/>
-        <Charts data={data} country={country}/>
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Charts data={data} country={country} />
       </div>
     );
   }
